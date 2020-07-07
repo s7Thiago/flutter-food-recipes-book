27746da34974a9a4a814be0323c7c9c1b740a45c
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-07 12:17:07 -0300

    Improving meal detail screen - part 2

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -7,50 +7,82 @@ class MealDetatilScreen extends StatelessWidget {
     // Obtaining the Meal object from arguments passed by route
     final meal = ModalRoute.of(context).settings.arguments as Meal;
 
+    _createSectionTitle(BuildContext context, String title) {
+      return Container(
+        margin: const EdgeInsets.only(top: 5),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget _createSectionContainer({Widget child}) {
+      return Container(
+        height: 250,
+        width: 330,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: child,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(meal.imageUrl),
-          ),
-          Container(
-            child: Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.headline6,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(meal.imageUrl),
             ),
-          ),
-          Container(
-            height: 250,
-            width: 300,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
+            _createSectionTitle(context, 'Ingredients'),
+            _createSectionContainer(
+              child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 10,
+                        ),
+                        child: Text(meal.ingredients[index])),
+                    color: Theme.of(context).accentColor,
+                  );
+                },
               ),
-              borderRadius: BorderRadius.circular(10),
             ),
-            child: ListView.builder(
-              itemCount: meal.ingredients.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 5,
-                        horizontal: 10,
+            _createSectionTitle(context, 'Steps'),
+            _createSectionContainer(
+              child: ListView.builder(
+                itemCount: meal.steps.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text('${index + 1}'),
+                        ),
+                        title: Text(meal.steps[index]),
                       ),
-                      child: Text(meal.ingredients[index])),
-                  color: Theme.of(context).accentColor,
-                );
-              },
-            ),
-          ),
-        ],
+                      Divider()
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
