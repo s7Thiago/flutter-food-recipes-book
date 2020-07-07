24df65227d11d2d159b63c4105fb17af9d18342b
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-07 12:00:17 -0300

    Improving meal detail screen - part 1

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -9,10 +9,48 @@ class MealDetatilScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meal Detatil'),
+        title: Text(meal.title),
       ),
-      body: Center(
-        child: Text('Details of ${meal.title}'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(meal.imageUrl),
+          ),
+          Container(
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            height: 250,
+            width: 300,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      child: Text(meal.ingredients[index])),
+                  color: Theme.of(context).accentColor,
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
