33dead79b457dad735930d7aa91c2ff2cc8de05c
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-07 09:23:05 -0300

    Passing data between screens via builder

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Recipes'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Recipes by category'),
+        child: Text('Recipes by category ${category.id}'),
       ),
     );
   }
