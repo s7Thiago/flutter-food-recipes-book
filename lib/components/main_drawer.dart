03e61092f2502f58938a9ccedb39aa9ea71a1920
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-08 17:48:24 -0300

    Creating drawer

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget createItem(IconData icon, String label) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        label,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.bottomRight,
+            color: Theme.of(context).accentColor,
+            child: Text(
+              'Let\'s Cook?',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          createItem(Icons.restaurant, 'Meals'),
+          createItem(Icons.settings, 'Settings'),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'categories_meals_screen.dart';
 import 'favorite_screen.dart';
+import '../components/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -28,27 +29,29 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_screens[_selectedScreenIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _screens[_selectedScreenIndex]['screen'],
       bottomNavigationBar: BottomNavigationBar(
-          onTap: _selectScreen,
-          currentIndex: _selectedScreenIndex,
-          backgroundColor: Theme.of(context).primaryColor,
-          unselectedItemColor: Colors.white70,
-          selectedItemColor: Theme.of(context).accentColor,
-          // type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.category,
-                ),
-                title: Text('Category')),
-            BottomNavigationBarItem(
+        onTap: _selectScreen,
+        currentIndex: _selectedScreenIndex,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white70,
+        selectedItemColor: Theme.of(context).accentColor,
+        // type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
               icon: Icon(
-                Icons.star,
+                Icons.category,
               ),
-              title: Text('Favorites'),
+              title: Text('Category')),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.star,
             ),
-          ]),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
     );
   }
 }
